53b011c7e7357f755c4bdf0333c739f2e659e232
JefersonGomes <jefersongomesusa@gmail.com>
2024-09-18 11:41:34 -0300

    primeiros estudos

---
+++ src/aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+public class aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Jeferson";
+        String endereco = "Rua ...";
+        double salario = 2522.21;
+        int dia = 12;
+        int mes = 11;
+        int ano = 2024;
+
+        System.out.println("Eu " +nome+", morando no endereço "+endereco+",");
+        System.out.println("confirmo que recebi o salário de "+salario+", na data "+dia+"/"+mes+"/"+ano+".");
+    }
+}
---
+++ src/aulas/java/devdojo/aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package aulas.java.devdojo;
+
+public class aula02TiposPrimitivos {
+    public static void main(String[] args) {
+     //int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte =10;
+        short idadeShort =10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        char caractereDois = 10;
+        String nome ="Jeferson";
+
+
+     System.out.println("A idade da pessoa é "+idade+" anos!!!");
+        System.out.println("oi meu nome é "+nome+" prazer em te conhecer");
+
+    }
+}
